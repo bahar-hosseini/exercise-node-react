6efb6f6769471f408e91deb7f8e3062139cffb61
Bahar Hosseini <bahar.hosseini@gmail.com>
2022-12-07 21:57:25 -0500

    add get request to GitHub API and send data to /repos

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,20 @@
+//External Modules
 import { Router, Request, Response } from 'express';
-
+import axios from 'axios';
+import { stringify } from 'flatted';
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
   res.status(200);
-
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  try {
+    const response = await axios.get(
+      'https://api.github.com/users/silverorange/repos'
+    );
+    res.send(stringify(response));
+  } catch (err) {
+    console.error(err);
+  }
 });
