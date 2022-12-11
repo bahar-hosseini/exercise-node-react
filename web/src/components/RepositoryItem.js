e1cdda3195667fc823f685518ffea2067b191f8f
Bahar Hosseini <bahar.hosseini@gmail.com>
2022-12-11 11:49:31 -0500

    handle unique "key" prop warning

--- web/src/components/RepositoryItem.js
+++ web/src/components/RepositoryItem.js
@@ -4,7 +4,10 @@ import './styles/RepositoryItem.css';
 const RepositoryItem = ({ name, language, description, forksCount }) => {
   return (
     <div className="container">
-      <div className="repoContainer">
+      <div
+        className="repoContainer"
+        onClick={() => alert("It's now Clickable")}
+      >
         <h3>name: {name}</h3>
         <h3>language: {language}</h3>
         <h3>forks count:{forksCount}</h3>
